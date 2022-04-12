270a5fc046b176db1310ea45e55c829fa824ac8e
Umberto Addesa <umberto.addesa@gmail.com>
2022-04-12 17:28:35 +0200

    changes from umberto on CiaoMondo
    I delete some comment

--- CiaoMondo.java
+++ CiaoMondo.java
@@ -5,18 +5,6 @@ public class CiaoMondo {
 	public static void main(String[] args) {
 		int voto = 27;
 		String saluto = "Hello Word";
-		//Dichiarazione  Assegnazione
-		//Esattamente come gli altri linguaggi
-		/*
-		 * questi non possono essere oggetti quindi non ci sara' una classe che li descrive
-		 * interi: byte 8bit, short 16 bit, int 32 bit, long 64 bit;
-		 * reali: float 32bit, double 64 bit
-		 * char per i caratteri
-		 * boolean t/f 
-		 * 
-		 * Tutti gli altri tipi possiedono una classe che li descrive con delle funzioni proprie o che posso aggiungere e creare
-		 */
-		
 		System.out.print(saluto);
 		System.out.println(saluto);
 		System.out.println("Posso scrivere liberamente" + ": " + voto); //Concatenare con +
